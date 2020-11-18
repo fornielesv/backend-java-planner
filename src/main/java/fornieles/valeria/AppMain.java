f7ad3cd9053ff175753507d90bb337352d985694
Valeria Fornieles <valeriafornieles@gmail.com>
2020-11-18 20:36:15 -0300

    add Javalin and logger

--- src/main/java/fornieles/valeria/AppMain.java
+++ src/main/java/fornieles/valeria/AppMain.java
@@ -1,7 +1,11 @@
 package fornieles.valeria;
 
+import io.javalin.Javalin;
+
 public class AppMain {
     public static void main(String[] args) {
-
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
+
